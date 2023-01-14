76e077e47c52b111abf181a0cac185d67749b69e
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2023-01-13 22:14:17 -0500

    general template for skills

---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+    {id: 125223, skill: 'HTML', done: true},
+    {id: 127904, skill: 'CSS', done: true},
+    {id: 139608, skill: 'Javascript' , done: true}
+  ];
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
